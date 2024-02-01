fe68ec6e346bc13090419f471d95f7da63a51217
Harvey Bland <harvey.bland@totum.com>
2024-02-01 21:22:39 +0000

    feat: passed data into document component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import { Item } from "../../types/types";
+import { File } from "../../types/types";
 import { jsonData } from "./api/json";
+import Document from "./components/Document";
 
 export default async function Home() {
   // Fetch json data from api/json.ts
-  const documents: Item[] = await jsonData();
-  console.log(documents);
+  const documents: File[] = await jsonData();
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Documents</h1>
+      <div>
+        {documents.map((document) => (
+          <div key={document.name}>
+            <Document
+              type={document.type}
+              name={document.name}
+              added={document.added}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
