40d425eaad57a71a418068bd146eaef34eb10bb9
Harvey Bland <harvey.bland@totum.com>
2024-02-01 20:43:26 +0000

    feat: added json api with types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
+import { Item } from "../../types/types";
+import { jsonData } from "./api/json";
+
+export default async function Home() {
+  // Fetch json data from api/json.ts
+  const documents: Item[] = await jsonData();
+  console.log(documents);
+
   return <main>Hello World</main>;
 }
---
+++ types/types.ts
@@ -0,0 +1,13 @@
+export interface File {
+    type: string;
+    name: string;
+    added: string;
+}
+
+export interface Folder {
+    type: "folder";
+    name: string;
+    files: File[];
+}
+
+export type Item = File | Folder;
