413a724628a9686c87d3df4820b48f885a81287b
Harvey Bland <harvey.bland@totum.com>
2024-02-02 14:37:46 +0000

    feat: filter folder files by document name

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import Image from "next/image";
-import { ModalProps } from "../../../types/types";
+import { File, ModalProps } from "../../../types/types";
 import DocumentInfo from "./DocumentInfo";
 import closeIcon from "../../../public/close-icon.svg";
+import { useEffect, useState } from "react";
+import OptionBar from "./OptionBar";
 
 export default function Modal({ files, closeModal }: ModalProps) {
+  const [filteredFiles, setFilteredFiles] = useState<File[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // Filter folder documents based on document name
+    const filtered = files.filter((file) => {
+      const nameMatches = file.name
+        ?.toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      return nameMatches;
+    });
+
+    setFilteredFiles(filtered);
+  }, [searchTerm, files]);
+
   return (
     <div
       className={`absolute flex flex-col items-center justify-center left-1/2 transform -translate-x-1/2 w-[500px] h-[300px] bg-white border rounded py-4 px-4`}
     >
+      <OptionBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <button
         type="button"
         className="absolute top-2 right-2 text-gray-500 cursor-pointer"
@@ -17,7 +35,7 @@ export default function Modal({ files, closeModal }: ModalProps) {
       >
         <Image src={closeIcon} alt="Close icon" width={20} height={20} />
       </button>
-      {files.map((file) => (
+      {filteredFiles.map((file) => (
         <div key={file.name}>
           <DocumentInfo type={file.type} name={file.name} added={file.added} />
         </div>
