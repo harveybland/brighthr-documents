e24023e48200f1cc18ca40c6f50be28e07b986ba
Harvey Bland <harvey.bland@totum.com>
2024-02-03 20:00:29 +0000

    feat: created types for the sort function

--- utils/sortFunction.ts
+++ utils/sortFunction.ts
@@ -1,9 +1,17 @@
-export default function sortDocuments(sortBy: string, state: any, setState: React.Dispatch<React.SetStateAction<any>>)  {
+import { DocumentItem, File } from "../types/types";
+
+// Create a type for the document or file array
+type SortedTypeArray = DocumentItem[] | File[]
+
+// Created a type for document or file 
+type SortedType = DocumentItem | File
+
+export default function sortDocuments(sortBy: string, state: SortedTypeArray, setState: React.Dispatch<React.SetStateAction<any>>)  {
     const sorted = [...state];
   
     switch (sortBy) {
       case "date":
-        sorted.sort((a: any, b: any) => {
+        sorted.sort((a: SortedType, b: SortedType) => {
           const dateA = a.added ? new Date(a.added) : null;
           const dateB = b.added ? new Date(b.added) : null;
           return (dateA?.getTime() || 0) - (dateB?.getTime() || 0);
